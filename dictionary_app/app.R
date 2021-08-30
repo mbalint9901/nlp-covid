bc2ac39d0a7d85bff1f046ea170671320f3482ce
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-31 00:37:03 +0200

    remove redundant ::

--- dictionary_app/app.R
+++ dictionary_app/app.R
@@ -1,39 +1,41 @@
 library(shiny)
+library(shinydashboard)
 library(tidyverse)
+
 sentence_sample <- read_rds("sentence_sample.RDS")
 
 
-ui <- shinydashboard::dashboardPage(
-  shinydashboard::dashboardHeader(title = "Words check"),
-    shinydashboard::dashboardSidebar(
-      shiny::selectInput(choices = sort(unique(filter(sentence_sample, !is.na(bing_sentiment) & bing_sentiment != new_sentiment)$words)), label = "Word:", inputId = "selected_word")
+ui <- dashboardPage(
+  dashboardHeader(title = "Words check"),
+    dashboardSidebar(
+      selectInput(choices = sort(unique(filter(sentence_sample, !is.na(bing_sentiment) & bing_sentiment != new_sentiment)$words)), label = "Word:", inputId = "selected_word")
   ),
-  shinydashboard::dashboardBody(
-    shinydashboard::box(width = NULL,
-                        shinydashboard::infoBoxOutput("n_occu", width = 6)
+  dashboardBody(
+    box(width = NULL,
+                        infoBoxOutput("n_occu", width = 6)
                         ),
-    shinydashboard::box(
+    box(
       width = NULL, title = "Examples sentences",
-  shiny::tableOutput("sentences")
+  tableOutput("sentences")
     )
   )
 
 )
 
 server <- function(input, output, session) {
-  output$sentences <- shiny::renderTable({
+  output$sentences <- renderTable({
     sentence_sample %>%
     filter(words  == input$selected_word) %>%
     select(Examples = sentences)
   })
   
-  output$n_occu <- shinydashboard::renderInfoBox({
+  output$n_occu <- renderInfoBox({
     sentence_sample %>%
     filter(words  == input$selected_word) %>% 
       nrow() %>%
       {./nrow(sentence_sample)} %>%
       {scales::percent(., accuracy = .01)}%>% 
-      shinydashboard::infoBox(title = "Rate of affected sentences")
+      infoBox(title = "Rate of affected sentences")
   }
   )
 }
--- dictionary_app/sample_setup.R
+++ dictionary_app/sample_setup.R
@@ -1,20 +1,40 @@
 library(tidyverse)
-load("C:/rprojects/CoronaSentiment/dat.RData")
+library(tidytext)
 
-sen_bing <- tidytext::get_sentiments("bing") %>% 
-  filter(sentiment %in% c("positive", "negative")) %>% 
-  mutate(bing_sentiment = ifelse(sentiment == "positive", 1, -1)) %>% 
-  select(words = word, bing_sentiment)
+WD <- getwd() %>% 
+  gsub(pattern = "nlp-covid.*", replacement = "nlp-covid")
 
-modified_bing <- readxl::read_excel("C:/rprojects/CoronaSentiment/bing_to_score.xlsx") %>% 
-  select(words = word, value = 'my sentiment') %>% 
-  mutate(new_sentiment = as.numeric(value)) %>% 
-  na.omit()
+dat <- list.files(str_c(WD, "/data/")) %>% 
+  keep(~ str_detect(., "dat_\\d.RDS")) %>% 
+  {str_c(WD, "/data/", .)} %>% 
+  map(readRDS) %>% 
+  reduce(rbind)
 
-sentence_sample <- dat %>% 
-  sample_n(50000) %>% 
+dat %>%
+  select(text) %>% 
+  unnes
+
+bing_comparison <- read_delim(str_c(WD, "/data/sentiment_scores.csv"), 
+                              ";", escape_double = FALSE, trim_ws = TRUE)
+
+load(str_c(WD, "/data/topics_bydat.RData"))
+
+topic_name <- tibble(
+  topic = 1:12, topic_name = c(
+    "Researches", "Politics", "Statistics", "Public Life", "Public Institutions", 
+    "Economy", "Restrictions", "Vaccination", "Sport", "Travel", "Police Measures", 
+    "Mental Health"
+  )
+)
+
+# sentence_sample <- 
+  dat %>% 
+  left_join(select(dat_topics, URL, topic = top_topic)) %>% 
+  left_join(topic_name) %>% 
+  group_by(topic_name) %>% 
+  group_modify(~ sample_n(.x, 10000, replace = TRUE)) %>% 
   tidytext::unnest_tokens(sentences, text, token = "sentences") %>% 
-  filter(!duplicated(URL)) %>% 
+  distinct()
   tidytext::unnest_tokens(words, sentences, drop = F) %>% 
   left_join(sen_bing) %>% 
   left_join(modified_bing)
