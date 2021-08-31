17a1c387c8b9da9faafc9f676edfb67d60318293
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-31 03:02:47 +0200

    update app

--- dictionary_app/sample_setup.R
+++ dictionary_app/sample_setup.R
@@ -23,19 +23,28 @@ topic_name <- tibble(
   )
 )
 
-sentence_sample <-dat %>% 
+sentence_sample <- dat %>% 
   left_join(select(dat_topics, URL, topic = top_topic)) %>% 
   left_join(topic_name) %>% 
   select(topic_name, text) %>% 
-  sample_frac(.1) %>% # ensure randomness
-  # TODO incrase rate to 1
-  tidytext::unnest_tokens(word, text, drop = F) %>% 
+  sample_frac(1) %>% # ensure randomness
+  # FIXME cannot allocate vector of size 1.3 Gb
+  unnest_tokens(sentence, text, "sentences") %>% 
+  unnest_tokens(word, sentence, drop = FALSE) %>% 
   right_join(bing_df) %>% 
   group_by(topic_name, word) %>% 
-  group_modify(~ head(.x, 10)) %>%
-  ungroup() %>% 
-  unnest_tokens(sentence, text, token = "sentences") %>% 
-  filter(str_detect(sentence, word)) %>% 
-  select(-sentiment)
+  group_modify(~ head(.x, 10)) %>% 
+  select(topic_name, word, sentence) %>% 
+  ungroup()
 
 saveRDS(sentence_sample, str_c(WD, "/dictionary_app/sentence_sample.RDS"))
+
+bing_freq <- readRDS(str_c(WD, "/data/dat_words_monthly.RDS")) %>% 
+  rename(word = words) %>% 
+  right_join(bing_df) %>% 
+  group_by(word) %>% 
+  summarise(n = sum(n, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  arrange(desc(n))
+
+saveRDS(bing_freq, str_c(WD, "/dictionary_app/bing_freq.RDS"))
